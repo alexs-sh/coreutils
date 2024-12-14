6ef99216c06b7180de6714afc87cc21b271aa3bb
Alexander Shirokov <shirokovalexs@gmail.com>
2024-12-14 18:12:07 +0100

    seq:floatparse:adjust spell-checker
    Issue #6935

--- src/uu/seq/src/seq.rs
+++ src/uu/seq/src/seq.rs
@@ -2,7 +2,7 @@
 //
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
-// spell-checker:ignore (ToDO) extendedbigdecimal numberparse
+// spell-checker:ignore (ToDO) extendedbigdecimal numberparse floatparse
 use std::io::{stdout, ErrorKind, Write};
 
 use clap::{crate_version, Arg, ArgAction, Command};
